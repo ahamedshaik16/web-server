db923ebfd15b229c9d47d286b73a0c55ce59a45a
Shuhail Ahamed <sohailcourses@gmail.com>
2016-12-12 20:15:12 +0530

    Refactor middleware

--- server.js
+++ server.js
@@ -7,17 +7,7 @@ var PORT = 3000;
 // 	res.send('Hello Express!');
 // });
 
-var middleware = {
-	requireAuthentication : function (req, res, next) {
-		console.log('private route hit');
-		next();
-	},
-	logger : function (req, res, next) {
-		 
-		console.log("Request: " + new Date().toString() + req.method + " " + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 // app.use(middleware.requireAuthentication);
